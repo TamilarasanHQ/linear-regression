e0383af21160dd2946fe0dd8dc37ffc408f6428a
tamilarasanhq <tamilarasanhq@gmail.com>
2026-06-06 19:41:16 +0530

    Save changes

--- sales_dataset_365.py
+++ sales_dataset_365.py
@@ -1,4 +1,3 @@
-#GENERATE THE DAILY SALES ARRAY
 import numpy as np
 import matplotlib.pylab as plt
 day = np.arange(365)
@@ -7,7 +6,6 @@ seasonality = np.linspace(0, 2*np.pi*52, 365)
 season = 20*np.sin(seasonality)
 noise = np.random.normal(loc=0, scale=15, size=365)
 sales = trend + season + noise
-#BASIC BUSINESS QUESTIONS
 total_daily_sales = np.sum(sales)
 avg_daily_sales = np.mean(sales)
 best_i = np.argmax(sales)
@@ -16,7 +14,6 @@ best_sales = sales[best_i]
 worst_sales = sales[worst_i]
 month_list = [31,28,31,30,31,30,31,31,30,31,30,31]
 cum_month = np.cumsum(month_list)
-# i can make this to a function later
 m = 0
 sum = 0
 monthly_totals = []
